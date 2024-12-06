14879fbf9eb93b4618bdbcf7497a2db929a5e948
황승민 <84918205+sxunxin@users.noreply.github.com>
2024-12-06 20:17:47 +0900

    [Gold I] Title: K번째 수, Time: 128 ms, Memory: 14352 KB -BaekjoonHub

---
+++ 백준/Gold/1300. K번째 수/K번째 수.java
@@ -0,0 +1,30 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Main {
+    // K번째 수 - https://www.acmicpc.net/problem/1300
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int N = Integer.parseInt(br.readLine());
+        int K = Integer.parseInt(br.readLine());
+
+        long lo = 1;
+        long hi = K;
+
+        while (lo < hi) {
+            long mid = (lo + hi) / 2;
+            long cnt = 0;
+
+            for (int i = 1; i <= N; i++) {
+                cnt += Math.min(mid / i, N);
+            }
+
+            if (K <= cnt) hi = mid;
+            else lo = mid + 1;
+        }
+
+        System.out.println(lo);
+    }
+}
