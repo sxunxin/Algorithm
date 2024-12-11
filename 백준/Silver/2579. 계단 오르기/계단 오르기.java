f964d162361e32f55ab17d3a48371d83a1edf20c
황승민 <84918205+sxunxin@users.noreply.github.com>
2024-12-11 14:22:17 +0900

    [Silver III] Title: 계단 오르기, Time: 104 ms, Memory: 14172 KB -BaekjoonHub

---
+++ 백준/Silver/2579. 계단 오르기/계단 오르기.java
@@ -0,0 +1,35 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Main {
+    // 계단 오르기 - https://www.acmicpc.net/problem/2579
+
+    static Integer[] dp;
+    static int[] arr;
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int N = Integer.parseInt(br.readLine());
+        dp = new Integer[N+1];
+        arr = new int[N+1];
+
+        for (int i = 1; i <= N; i++)  {
+            arr[i] = Integer.parseInt(br.readLine());
+        }
+        dp[0] = arr[0];
+        dp[1] = arr[1];
+
+        if (N >= 2) {
+            dp[2] = arr[1] + arr[2];
+        }
+
+        System.out.println(stairs(N));
+    }
+
+    static int stairs(int N) {
+        if (dp[N] == null) {
+            dp[N] = Math.max(stairs(N-2), stairs(N-3) + arr[N-1]) + arr[N];
+        }
+        return dp[N];
+    }
+}
