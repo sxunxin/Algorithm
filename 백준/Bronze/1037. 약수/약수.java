fb21bea81011cae6ba74d24f9ad852687f9a88c7
황승민 <84918205+sxunxin@users.noreply.github.com>
2024-12-14 15:08:04 +0900

    [Bronze I] Title: 약수, Time: 216 ms, Memory: 17748 KB -BaekjoonHub

---
+++ 백준/Bronze/1037. 약수/약수.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+
+public class Main {
+    // 약수 - https://www.acmicpc.net/problem/1037
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        int N = scanner.nextInt();
+        int max = 0;
+        int min = 1000000;
+
+        for (int i = 0; i < N; i++) {
+            int temp = scanner.nextInt();
+            if (temp > max) max = temp;
+            if (temp < min) min = temp;
+        }
+        System.out.println(max * min);
+
+    }
+}
