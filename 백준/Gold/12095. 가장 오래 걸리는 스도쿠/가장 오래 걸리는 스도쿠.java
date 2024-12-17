bb3d1ffaa2120b9e835b1c212d52872cc001dfd2
황승민 <84918205+sxunxin@users.noreply.github.com>
2024-12-17 15:00:14 +0900

    [Gold III] Title: 가장 오래 걸리는 스도쿠, Time: 88 ms, Memory: 13852 KB, Score: 102108 point -BaekjoonHub

---
+++ 백준/Gold/12095. 가장 오래 걸리는 스도쿠/가장 오래 걸리는 스도쿠.java
@@ -0,0 +1,17 @@
+public class Main {
+    // 가장 오래 걸리는 스도쿠 - https://www.acmicpc.net/problem/12095
+
+    public static void main(String[] args) {
+        System.out.println(
+                        "8 0 0 0 0 0 0 0 0\n" + 
+                        "0 0 3 6 0 0 0 0 0\n" +
+                        "0 7 0 0 9 0 2 0 0\n" +
+                        "0 5 0 0 0 7 0 0 0\n" +
+                        "0 0 0 0 4 0 7 0 0\n" +
+                        "0 0 0 1 0 0 0 3 0\n" +
+                        "0 0 1 0 0 0 0 0 8\n" +
+                        "0 0 8 5 0 0 0 1 0\n" +
+                        "0 9 0 0 0 0 4 0 0"
+        );
+    }
+}
