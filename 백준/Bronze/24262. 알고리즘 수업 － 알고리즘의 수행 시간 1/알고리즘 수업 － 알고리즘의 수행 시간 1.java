13e11cb953c0b8e4ff392443c51b39ab087ea49e
황승민 <84918205+sxunxin@users.noreply.github.com>
2024-12-21 11:18:54 +0900

    [Bronze V] Title: 알고리즘 수업 - 알고리즘의 수행 시간 1, Time: 100 ms, Memory: 14056 KB -BaekjoonHub

---
+++ 백준/Bronze/24262. 알고리즘 수업 － 알고리즘의 수행 시간 1/알고리즘 수업 － 알고리즘의 수행 시간 1.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+
+public class Main {
+    // 알고리즘 수업 - 알고리즘의 수행 시간 1 - https://www.acmicpc.net/problem/24262
+
+    public static void main(String[] args) {
+        System.out.println(1);
+        System.out.println(0);
+    }
+    
+}
