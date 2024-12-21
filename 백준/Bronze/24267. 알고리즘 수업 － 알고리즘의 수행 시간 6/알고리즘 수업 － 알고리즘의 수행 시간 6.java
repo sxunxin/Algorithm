f2662a273139fb639ac654c70943d243b8ff1b05
황승민 <84918205+sxunxin@users.noreply.github.com>
2024-12-21 11:20:12 +0900

    [Bronze II] Title: 알고리즘 수업 - 알고리즘의 수행 시간 6, Time: 164 ms, Memory: 17684 KB -BaekjoonHub

---
+++ 백준/Bronze/24267. 알고리즘 수업 － 알고리즘의 수행 시간 6/알고리즘 수업 － 알고리즘의 수행 시간 6.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+
+public class Main {
+    // 알고리즘 수업 - 알고리즘의 수행 시간 6 - https://www.acmicpc.net/problem/24267
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        long n = scanner.nextInt();
+        System.out.println((n-1)*(n-2)*n/6);
+        System.out.println(3);
+    }
+    
+}
