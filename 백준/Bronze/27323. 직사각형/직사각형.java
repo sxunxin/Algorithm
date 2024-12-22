ca3e43f0c7cb968c098e28bc373095f3625af400
황승민 <84918205+sxunxin@users.noreply.github.com>
2024-12-22 16:26:32 +0900

    [Bronze V] Title: 직사각형, Time: 212 ms, Memory: 17728 KB -BaekjoonHub

---
+++ 백준/Bronze/27323. 직사각형/직사각형.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+
+public class Main {
+    // 직사각형 - https://www.acmicpc.net/problem/27323
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int a = scanner.nextInt();
+        int b = scanner.nextInt();
+
+        System.out.println(a*b);
+    }
+}
