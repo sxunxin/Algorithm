b18cfb084870c0e60fd5149bff66ef80b982b6e6
황승민 <84918205+sxunxin@users.noreply.github.com>
2024-12-26 11:07:42 +0900

    [Bronze III] Title: 별 찍기 - 7, Time: 0 ms, Memory: 1112 KB -BaekjoonHub

---
+++ 백준/Bronze/2444. 별 찍기 － 7/별 찍기 － 7.cc
@@ -0,0 +1,29 @@
+#include <stdio.h>
+
+int main(void) {
+
+    int n;
+    scanf("%d", &n);
+
+    for (int i = 1; i <= n; i++) {
+        for (int j = 1; j <= n-i; j++) {
+            printf(" ");
+        }
+        for (int j = 1; j <= i*2-1; j++) {
+            printf("*");
+        }
+        printf("\n");
+    }
+
+    for (int i = n - 1; i >= 1; i--) {
+        for (int j = 1; j <= n - i; j++) {
+            printf(" ");
+        }
+        for (int j = 1; j <= i*2 - 1; j++) {
+            printf("*");
+        }
+        printf("\n");
+    }
+
+    return 0;
+}
