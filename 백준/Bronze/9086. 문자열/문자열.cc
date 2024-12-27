ecaebc62d3868bad4f4719e21ac80cb9c65d4c65
황승민 <84918205+sxunxin@users.noreply.github.com>
2024-12-27 10:31:10 +0900

    [Bronze V] Title: 문자열, Time: 0 ms, Memory: 1112 KB -BaekjoonHub

---
+++ 백준/Bronze/9086. 문자열/문자열.cc
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <string.h>
+
+int main(void) {
+
+    int t = 0;
+    char arr[1001];
+    scanf("%d", &t);
+
+    for (int i = 0; i < t; i++) {
+        scanf("%s", arr);
+        printf("%c%c\n", arr[0], arr[strlen(arr)-1]);
+    }
+
+    return 0;
+}
