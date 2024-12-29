2e0bce84e02cd4566c017bfb808315f2a7639fd9
황승민 <84918205+sxunxin@users.noreply.github.com>
2024-12-29 12:57:18 +0900

    [Bronze V] Title: 코딩은 체육과목 입니다, Time: 0 ms, Memory: 1112 KB -BaekjoonHub

---
+++ 백준/Bronze/25314. 코딩은 체육과목 입니다/코딩은 체육과목 입니다.cc
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main(void) {
+
+    int n;
+    scanf("%d", &n);
+    n = n / 4;
+
+    for (int i = 1; i <= n; i++) {
+        printf("long ");
+    }
+    printf("int");
+    return 0;
+
+}
