aba01cbbae425400cc37d00efaebf7fb08bbbe53
황승민 <84918205+sxunxin@users.noreply.github.com>
2024-12-29 12:57:26 +0900

    [Bronze IV] Title: 빠른 A+B, Time: 356 ms, Memory: 1112 KB -BaekjoonHub

---
+++ 백준/Bronze/15552. 빠른 A＋B/빠른 A＋B.cc
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main(void) {
+
+    int n, a, b;
+    scanf("%d", &n);
+
+    for (int i = 1; i <= n; i++ ) {
+        scanf("%d %d", &a, &b);
+        printf("%d\n", a+b);
+    }
+
+    return 0;
+
+}
