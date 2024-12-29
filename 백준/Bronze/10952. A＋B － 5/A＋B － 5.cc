f691262333506fb4930039dc9f718b15b4791664
황승민 <84918205+sxunxin@users.noreply.github.com>
2024-12-29 12:58:13 +0900

    [Bronze V] Title: A+B - 5, Time: 0 ms, Memory: 1112 KB -BaekjoonHub

---
+++ 백준/Bronze/10952. A＋B － 5/A＋B － 5.cc
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+int main(void) {
+
+    int a, b;
+    while (a + b != 0)
+    {
+        scanf("%d %d", &a, &b);
+        if (a + b != 0) {
+            printf("%d\n", a + b);
+
+        }
+    }
+    
+
+    return 0;
+
+}
