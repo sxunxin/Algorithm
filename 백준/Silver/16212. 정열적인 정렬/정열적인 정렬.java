4fd332d8eef48c25050ce3db590109831cc94c9e
황승민 <84918205+sxunxin@users.noreply.github.com>
2025-06-18 17:14:35 +0900

    [Silver V] Title: 정열적인 정렬, Time: 1172 ms, Memory: 83532 KB, Score: 25 point -BaekjoonHub

---
+++ 백준/Silver/16212. 정열적인 정렬/정열적인 정렬.java
@@ -0,0 +1,28 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.StringTokenizer;
+
+public class Main {
+    // 정열적인 정렬 - https://www.acmicpc.net/problem/16212
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
+
+        int N = Integer.parseInt(br.readLine());
+        int[] arr = new int[N];
+
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        for (int i = 0; i < N; i++) {
+            arr[i] = Integer.parseInt(st.nextToken());
+        }
+
+        Arrays.sort(arr);
+        for (int i = 0; i < N; i++) {
+            sb.append(arr[i]).append(" ");
+        }
+        System.out.println(sb);
+    }
+}
