cf1a11a2327c1b9ce19786b48641ddebc5eba4e9
황승민 <84918205+sxunxin@users.noreply.github.com>
2025-09-09 17:12:46 +0900

    [Gold IV] Title: 테트로미노, Time: 652 ms, Memory: 34376 KB -BaekjoonHub

---
+++ 백준/Gold/14500. 테트로미노/테트로미노.java
@@ -0,0 +1,62 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+    // 테트로미노 - https://www.acmicpc.net/problem/14500
+
+    static int N, M;
+    static int[][] map;
+    static boolean[][] visited;
+    static int max = Integer.MIN_VALUE;
+
+    static int[] dx = {-1, 1, 0, 0};
+    static int[] dy = {0, 0, -1, 1};
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        N = Integer.parseInt(st.nextToken());
+        M = Integer.parseInt(st.nextToken());
+
+        map = new int[N][M];
+        visited = new boolean[N][M];
+
+        for (int i = 0; i < N; i++) {
+            st = new StringTokenizer(br.readLine());
+            for (int j = 0; j < M; j++) {
+                map[i][j] = Integer.parseInt(st.nextToken());
+            }
+        }
+
+        for (int i = 0; i < N; i++) {
+            for (int j = 0; j < M; j++) {
+                visited[i][j] = true;
+                dfs(i, j, 1, map[i][j]);
+                visited[i][j] = false;
+            }
+        }
+
+        System.out.println(max);
+    }
+
+    private static void dfs(int x, int y, int cnt, int sum) {
+        if (cnt == 4) {
+            max = Math.max(max, sum);
+            return;
+        }
+        for (int i = 0; i < 4; i++) {
+            int nx = x + dx[i];
+            int ny = y + dy[i];
+
+            if (nx < 0 || nx >= N || ny < 0 || ny >= M || visited[nx][ny]) continue;
+
+            visited[nx][ny] = true;
+            if (cnt == 2) dfs(x, y, cnt + 1, sum + map[nx][ny]);
+            dfs(nx, ny, cnt + 1, sum + map[nx][ny]);
+            visited[nx][ny] = false;
+        }
+    }
+}
